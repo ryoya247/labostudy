cf9e7bee9d09970b5c1b8df38f2f1ca420facd74
ryoya247 <chan1no.hello.16@gmail.com>
2018-05-11 12:14:05 +0900

    サインイン・アウトの実装

--- myapp2/public/src/components/MainPage.vue
+++ myapp2/public/src/components/MainPage.vue
@@ -1,9 +1,35 @@
 <template>
   <div class = 'mainpage'>
     <h1>this is mainpage</h1>
+    <b-button @click='onSignOut'>signOut</b-button>
   </div>
 </template>
 <script>
+import firebase from 'firebase'
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'getUserId',
+      'getCurrentUserInfo'
+    ])
+  },
+  mounted () {
+    console.log(this.getCurrentUserInfo)
+  },
+  methods: {
+    onSignOut () {
+      firebase.auth().signOut().then(
+        (user) => {
+          console.log('*** sign out ***')
+          console.log(user)
+          this.$router.replace({ name: 'SignIn' })
+        }
+      )
+    }
+  }
+}
 
 </script>
 <style scoped>
